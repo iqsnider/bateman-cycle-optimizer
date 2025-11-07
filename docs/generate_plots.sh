b302c3b41a078134925a030c14881db126e43247
Ian Snider <iqsnider@gmail.com>
2025-11-07 09:53:39 -0800

    feat: bash for plots

---
+++ docs/generate_plots.sh
@@ -0,0 +1,2 @@
+uv run optim --time 10 --rate 0.99 --tp 0.619 --td 1.411 --a 148 --save docs/figs/A148.png
+uv run optim --time 10 --rate 10.98 --tp 0.894 --td 4.06 --a 147 --save docs/figs/A147.png
